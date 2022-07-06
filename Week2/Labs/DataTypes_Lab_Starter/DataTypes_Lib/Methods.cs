bfcb4530b2995c742dc709e895d076a0e381d5d5
LabaDev <labalimbu360@gmail.com>
2022-07-06 22:22:16 +0100

    Uploaded and Completed DataTypes_lib

---
+++ Week2/Labs/DataTypes_Lab_Starter/DataTypes_Lib/Methods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DataTypes_Lib
+{
+    public static class Methods
+    {
+        // write a method to return the product of all numbers from 1 to n inclusive
+        public static int Factorial(int n)
+        {
+            int answer = 1;
+            for (int i = 1; i <=n; i++)
+            {
+                answer *= i;
+            }
+            return answer;
+        }
+
+        public static float Mult(float num1, float num2)
+        {
+            float number = num1* num2;
+            return (float)Math.Round(number, 3);
+            
+        }
+    }
+}
---
+++ Week2/Labs/DataTypes_Lab_Starter/DataTypes_Lib/TypeConversion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DataTypes_Lib
+{
+    public class TypeConversion
+    {
+        public static short UIntToShort(uint num)
+        {
+            if (num > short.MaxValue)
+                throw new OverflowException();
+            return (short)num;
+        }
+
+        public static long FloatToLong(float num)
+        {
+            if ((num - Math.Floor(num)) > 0)
+                num--;
+            else if ((num - Math.Floor(num)) > 0)
+                num++;
+            return (long)num;
+        }
+    }
+}
