650b92532379c77aa623f10f226c76a4ff5fd37a
LabaDev <labalimbu360@gmail.com>
2022-07-07 15:50:53 +0100

    Attempted array exercises

--- Week2/MoreTypes_Lab_Starter/MoreTypes_Lab/MoreTypes_Lib/ArraysExercises.cs
+++ Week2/MoreTypes_Lab_Starter/MoreTypes_Lab/MoreTypes_Lib/ArraysExercises.cs
@@ -8,19 +8,46 @@ namespace MoreTypes_Lib
         // returns a 1D array containing the contents of a given List
         public static string[] Make1DArray(List<string> contents)
         {
-            throw new NotImplementedException();
+            string[] array = new string[contents.Count];   
+            for (int i =0; i<contents.Count; i++)
+                array[i] = contents[i];
+            return array;
         }
 
         // returns a 3D array containing the contents of a given List
         public static string[,,] Make3DArray(int length1, int length2, int length3, List<string> contents)
         {
-            throw new NotImplementedException();
+            string[,,] array = new string[length1, length2, length3];
+            int j = 0;
+
+
+            for (int i = 0; i <= length1; i++)
+            {
+                for (int k = 0; k <= length3; k++)
+                {
+                    array[i, 0, k] = contents[j];
+                    j++;
+                }
+                
+            }
+
+            return array;
         }
 
         // returns a jagged array containing the contents of a given List
         public static string[][] MakeJagged2DArray(int countRow1, int countRow2, List<string> contents)
         {
-            throw new NotImplementedException();
-        }
+            string[][] jaggedArray = new string[2][];
+
+            int counter = 0;
+            for (int i = 0; i<countRow1; i++)
+                jaggedArray[0][i] = contents[counter++];
+            jaggedArray[1] = new string[countRow2];
+            for (int j = 0; j < countRow2; j++)
+            
+                jaggedArray[1][j] = contents[counter++];
+            
+            return jaggedArray;
+        }   
     }
 }
