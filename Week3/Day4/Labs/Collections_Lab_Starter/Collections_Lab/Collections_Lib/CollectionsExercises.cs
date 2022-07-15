c0bab85d6a85cbe500f02609dc65ba6c5dc46748
LabaDev <labalimbu360@gmail.com>
2022-07-15 14:22:11 +0100

    updated day5

--- Week3/Day4/Labs/Collections_Lab_Starter/Collections_Lab/Collections_Lib/CollectionsExercises.cs
+++ Week3/Day4/Labs/Collections_Lab_Starter/Collections_Lab/Collections_Lib/CollectionsExercises.cs
@@ -9,20 +9,52 @@ namespace Collections_Lib
         /* removes and returns the next num entries in the queue, as a comma separated string */
         public static string NextInQueue(int num, Queue<string> queue)
         {
+            var list = new List<string>();
+            string output = "";
+            if (num == 1)
+                return queue.Dequeue();
+
             
+
+            for (int i = 0; i < num-1; i++)
+                output += queue.Dequeue() + ", ";
+            output += queue.Dequeue();
+
+            if (queue.Count == 0)
+                return "";
+
+            return output;
         }
 
         /* uses a Stack to create and return array of ints in reverse order to the one supplied */
         public static int[] Reverse(int[] original)
         {
-            
+            return new int[] { };
         }
         // using a Dictionary, counts and returns (as a string) the occurence of the digits 0-9 in the given string
         public static string CountDigits(string input)
         {
-            string = "";
-            foreach(char c in input)
-                if ()
+            if (input == "")
+                return "";
+            var countDict = new Dictionary<char, int>();
+            foreach (char c in input)
+            {
+                if (Int32.TryParse(c.ToString(), out int result))
+                {
+                    if (countDict.ContainsKey(c))
+                        countDict[c]++;
+                    else
+                        countDict.Add(c, 1);
+                }
+            }
+            string output = "";
+            foreach (var entry in countDict)
+            {
+                output += entry;
+            }
+                
+                
+            return output;    
         }
     }
 }
