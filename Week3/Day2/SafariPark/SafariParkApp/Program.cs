e3e2af934410bab35f451e82815a8bd1b8bab2c4
LabaDev <labalimbu360@gmail.com>
2022-07-15 13:15:31 +0100

    day5

--- Week3/Day2/SafariPark/SafariParkApp/Program.cs
+++ Week3/Day2/SafariPark/SafariParkApp/Program.cs
@@ -11,8 +11,9 @@ namespace SafariParkApp
             //Console.WriteLine(jon.Age);
             //Console.WriteLine(jon.FullName());
             Person charlie = new Person("Charlie");
-            Person perter = new Person();
+            Person perter = new Person("Peter");
             Person dan = new Person("Dan", "Summerside") { Age = 100 };
+            Person Helen = new Person("Helen", "Troy") { Age = 22 };
             //Person laba = new Person { FirstName = "Laba", LastName = "Limbu", Age = 33};
 
             //var shop1 = new Shopping { Ties =3, Shirts =3 , Trousers = 1};
@@ -70,6 +71,85 @@ namespace SafariParkApp
             //{
             //    Console.WriteLine(item);
             //}
+
+
+            //Queues
+            //FIFO = First in First out
+            //var myQueue = new Queue<Person>();
+            //myQueue.Enqueue(charlie);
+            //myQueue.Enqueue(dan);
+            //myQueue.Enqueue(new Person("David"));
+
+            //Console.WriteLine("Queue\n");
+            //foreach (var person in myQueue)
+            //    Console.WriteLine(person);
+
+            //Console.WriteLine("\n");
+            //var first = myQueue.Peek();
+            //Console.WriteLine(first);
+
+            //Console.WriteLine("\n");
+            //var serve = myQueue.Peek();
+            //Console.WriteLine(serve);
+
+            //Stack
+            //LIFO = Last in First out
+            //var stack = new Stack<int>();
+            //int[] nums = new[] { 1, 2, 3, 4, 5, 6 };
+            //int[] numsReversed = new int[nums.Length];
+
+            //foreach(var num in nums)
+            //    stack.Push(num);
+            //for (int i = 0; i < numsReversed.Length; i++)
+            //    numsReversed[i] = stack.Pop();
+
+            //HashSet
+            //var peopleSet = new HashSet<Person>()
+            //{
+            //    charlie, new Person("Dylan"), new Person("Thomas"), new Person("Akon"), Helen
+            //};
+            //Console.WriteLine("Hash set");
+            //foreach (var entry in peopleSet)
+            //    Console.WriteLine(entry);
+
+            //var successMartin = peopleSet.Add(new Person("Martin", "Beard") { Age = 21}); 
+            //var successHelen = peopleSet.Add(new Person("Helen", "Troy") { Age = 22});
+            //Console.WriteLine(successHelen);
+            //Console.WriteLine(successMartin);
+
+            //var morePeople = new HashSet<Person> { new Person("Cathy"), new Person("Jasmine"), new Person("Thomas") };
+            //peopleSet.IntersectWith(morePeople);
+            //Console.WriteLine(peopleSet);
+
+            //Dictionary
+            //var personDictionary = new Dictionary<string, Person>()
+            //{
+            //    {"Helen", Helen },
+            //    {"Tom", new Person("Thomas") }
+            //};
+
+            //var p = personDictionary["Tom"];
+            //personDictionary.Add("Char", charlie);
+
+            //string input = "The cat in the hat comes back";
+            //input = input.Trim().ToLower();
+            //var countDict = new Dictionary<char, int>();
+            //foreach (char c in input)
+            //    if (countDict.ContainsKey(c))
+            //            countDict[c]++;
+            //    else
+            //        countDict.Add(c, 1);
+            //Console.WriteLine("\n Dictionary Problem");
+            //foreach (var entry in countDict)
+            //    Console.WriteLine(entry);
+            //foreach (var keys in countDict.Keys)
+
+            //    Console.WriteLine($"Key {keys}");
+            //Console.WriteLine();
+            //foreach (var value in countDict.Values)
+            //    Console.WriteLine($"Value {value}");
+
+
 
         }
         
--- Week3/Day4/Labs/Collections_Lab_Starter/Collections_Lab/Collections_Lib/CollectionsExercises.cs
+++ Week3/Day4/Labs/Collections_Lab_Starter/Collections_Lab/Collections_Lib/CollectionsExercises.cs
@@ -9,18 +9,20 @@ namespace Collections_Lib
         /* removes and returns the next num entries in the queue, as a comma separated string */
         public static string NextInQueue(int num, Queue<string> queue)
         {
-            throw new NotImplementedException();
+            
         }
 
         /* uses a Stack to create and return array of ints in reverse order to the one supplied */
         public static int[] Reverse(int[] original)
         {
-            throw new NotImplementedException();
+            
         }
         // using a Dictionary, counts and returns (as a string) the occurence of the digits 0-9 in the given string
         public static string CountDigits(string input)
         {
-            throw new NotImplementedException(); 
+            string = "";
+            foreach(char c in input)
+                if ()
         }
     }
 }
