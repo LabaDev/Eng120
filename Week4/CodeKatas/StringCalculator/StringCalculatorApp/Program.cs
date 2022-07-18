d2a98a7292358256004d4d737ad50505841badb8
LabaDev <labalimbu360@gmail.com>
2022-07-18 18:40:35 +0100

    Code Kata String Calculator added

---
+++ Week4/CodeKatas/StringCalculator/StringCalculatorApp/Program.cs
@@ -0,0 +1,131 @@
+﻿namespace StringCalculatorApp;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+
+    //public static int Calculator(string entry)
+    //{
+    //    if (entry.Length == 0)
+    //        return 0;
+    //}
+
+    //public static int Calculator(string entry)
+    //{
+    //    if (entry.Length == 0)
+    //        return 0;
+    //    var number = Int32.TryParse(entry, out var value);
+    //    return value;
+    //}
+
+    //public static int Calculator(string entry)
+    //{
+    //    int number = 0;
+    //    if (entry.Length == 0 || entry == "1,\n")
+    //        return number;
+    //    for (int i = 0; i < entry.Length; i++)
+    //    {
+    //        Int32.TryParse(entry[i].ToString(), out var value);
+    //        number += value;
+    //    }
+
+
+    //    return number;
+    //}
+
+
+    //public static int Calculator(string entry)
+    //{
+    //    int number = 0;
+    //    if (entry.Length == 0 || entry == "1,\n")
+    //        return number;
+    //    for (int i = 0; i < entry.Length; i++)
+    //    {
+    //        if (entry[i] == '-')
+    //            throw new ArgumentException("Negative Numbers not Allowed!");
+    //        Int32.TryParse(entry[i].ToString(), out var value);
+    //        number += value;
+    //    }
+
+
+    //    return number;
+    //}
+
+
+    //public static int Calculator(string entry)
+    //{
+    //    int number = 0;
+    //    if (entry.Length == 0 || entry == "1,\n")
+    //        return number;
+
+    //    entry += ' ';
+    //    bool containsNegative = false;
+    //    string intMoreThanOneChar = "";
+    //    for (int i = 0; i < entry.Length; i++)
+    //    {
+    //        //detecting negative numbers
+    //        if (entry[i] == '-')
+    //            containsNegative = true;
+
+    //        if (Int32.TryParse(entry[i].ToString(), out var value))
+    //            intMoreThanOneChar += value;
+    //        else
+    //        {
+    //            Int32.TryParse(intMoreThanOneChar, out var value2);
+    //            number += value2;
+    //            intMoreThanOneChar = "";
+    //        }
+    //    }
+
+    //    if (containsNegative)
+    //        throw new ArgumentException("Negative Numbers not Allowed!");
+
+    //    return number;
+    //}
+
+    public static int Calculator(string entry)
+    {
+        //Initializing the answer
+        int number = 0;
+
+        //Checking is entry is empty or is "1,\n"
+        if (entry.Length == 0 || entry == "1,\n")
+            return number;
+
+        //Adding space to the entry to check more values more than 9
+        entry += ' ';
+
+        //initailizing to find negative numbers.
+        bool containsNegative = false;
+
+        //initialing if entry contains number more than 9
+        string intMoreThanOneChar = "";
+
+        for (int i = 0; i < entry.Length; i++)
+        {
+            
+            if (entry[i] == '-')
+                containsNegative = true;
+
+
+            if (Int32.TryParse(entry[i].ToString(), out var value))
+                intMoreThanOneChar += value;
+            else
+            {
+                Int32.TryParse(intMoreThanOneChar, out var value2);
+                if (value2 > 1000)
+                    value2 = 0;
+                number += value2;
+                intMoreThanOneChar = "";
+            }
+        }
+
+        if (containsNegative)
+            throw new ArgumentException("Negative Numbers not Allowed!");
+
+        return number;
+    }
+}
---
+++ Week4/CodeKatas/StringCalculator/StringCalculatorTest/UnitTest1.cs
@@ -0,0 +1,79 @@
+namespace StringCalculatorTest
+{
+    public class Tests
+    {
+        
+
+        [Test]
+        public void GivenNothingToCalculator_ReturnsZero()
+        {
+            Assert.That(Program.Calculator(""), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GivenInputAsOne_Calculator_ReturnsOne()
+        { 
+            Assert.That(Program.Calculator("1"), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void GivenInputAsOneAndTwo_Calculator_ReturnsThree()
+        {
+            Assert.That(Program.Calculator("1,2"), Is.EqualTo(3));
+        }
+
+        [Test]
+        public void GivenInputAsOne_Slash_NTwoThree_Calculator_ReturnsSix()
+        {
+            Assert.That(Program.Calculator("1\n2,3"), Is.EqualTo(6));
+        }
+
+        [Test]
+        public void GivenInputAsOne_Slash_N_Calculator_Returns0()
+        {
+            Assert.That(Program.Calculator("1,\n"), Is.EqualTo(0));
+        }
+
+        
+        [Test]
+        public void GivenInputAsSlashSlashDelimiterSlashNOneAndTwo_N_Calculator_Returns3()
+        {
+            Assert.That(Program.Calculator("//;\n1;2"), Is.EqualTo(3));
+        }
+
+        [Test]
+        public void GivenInputAsNegativeNumbersOrNumber_Calculator_ThrowsArgumentError()
+        {
+            Assert.That(() => Program.Calculator("//;\n1;-2"), Throws.TypeOf<ArgumentException>().With.Message.Contains("Negative Numbers not Allowed!"));
+        }
+
+        [Test]
+        public void GivenInputAsHundred_Calculator_Retruns100()
+        {
+            Assert.That(() => Program.Calculator("100"), Is.EqualTo(100));
+        }
+
+        [Test]
+        public void GivenInputAsHundredAnd200_Calculator_Retruns300()
+        {
+            Assert.That(() => Program.Calculator("100,,,,,;;;;200"), Is.EqualTo(300));
+        }
+
+
+        [Test]
+        public void GivenInputAs1001And2_Calculator_Retruns2()
+        {
+            Assert.That(() => Program.Calculator("1001,,,,,;;;;2"), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void GivenMultipleDelimitersAsInputWithNumbersOneTwoAndThree_Calculator_Retruns6()
+        {
+            Assert.That(() => Program.Calculator("//[***]\n1***2***3"), Is.EqualTo(6));
+            Assert.That(() => Program.Calculator("//[*][%]\n1*2%3"), Is.EqualTo(6));
+        }
+
+        
+    }
+
+}
