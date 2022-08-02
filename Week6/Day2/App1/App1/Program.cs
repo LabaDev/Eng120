d12c7e9017130155f2787f07fcdf372d470afe29
Labahang Limbu <88229692+LabaDev@users.noreply.github.com>
2022-08-02 13:39:55 +0100

    Add files via upload

---
+++ Week6/Day2/App1/App1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SerialisationApp
+{
+    public class Program
+    {
+        private static readonly string _path  = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        private static readonly string _path2 = Path.Combine(Directory.GetCurrentDirectory(), @"../../../");
+        private static ISerialise? _serialiser;
+        static void Main(string[] args)
+        {
+            Trainee trainee = new Trainee
+            {
+                FirstName = "Bob",
+                LastName = "Marley",
+                SpartaNo = 100
+            };
+
+            var _serialiser = new SerialiserBinary();
+            _serialiser.SerialisToFile($"{_path2}/BinaryTrainee.bin", trainee);
+            //serialiser.SerialisToFile<Trainee>($"{_path}/BinaryTrainee.bin", trainee);
+
+            Trainee deserialisedBob = _serialiser.DeserialiserFromFile<Trainee>($"{_path2}/BinaryTrainee.bin");
+
+            if (trainee == deserialisedBob)
+                Console.WriteLine("Welcome back");
+            else
+                Console.WriteLine("Imposter!!");
+        }
+
+    }
+}
---
+++ Week6/Day2/App1/App1/SerialiserBinary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialisationApp
+{
+    internal class SerialiserBinary
+    {
+        public void SerialisToFile<T>(string filePath, T item)
+        {
+            //Stream or FileStream
+            FileStream fileStream  = File.Create(filePath);
+            //Create a BinaryFormatter object and use it tot serailase the item to file
+            BinaryFormatter writer = new BinaryFormatter();
+            //Serialise
+            writer.Serialize(fileStream, item);
+            fileStream.Close(); 
+        }
+
+        public T DeserialiserFromFile<T>(string filePath)
+        {
+           // Create a stram object for reading
+           Stream fileStream = File.OpenRead(filePath);
+            BinaryFormatter reader = new BinaryFormatter();
+            var deserialised = (T)reader.Deserialize(fileStream);
+            fileStream.Close();
+            return deserialised;
+
+
+
+        }
+    }
+}
